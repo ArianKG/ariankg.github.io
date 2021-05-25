3e6772709c490a8a8380054dee8242fa1c4e3c86
ArianKG <72304422+ArianKG@users.noreply.github.com>
2021-05-25 04:29:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ for (i = 0; i < 1; i++) {
     a[i] = document.createElement('a');
 }
 
-a[0].innerHTML = 'Random Number Generator';
-a[0].href = 'generators/random-number-generator.html';
+a[0].innerHTML = 'Generators';
+a[0].href = 'generators.html';
 
 for (i = 0; i < 1; i++) {
     document.body.appendChild(a[i]);
