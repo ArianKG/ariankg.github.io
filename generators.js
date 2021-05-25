3470b9f777a9ccb0d4c66014c2cc29e5aeaa53ad
ArianKG <72304422+ArianKG@users.noreply.github.com>
2021-05-25 04:28:18 -0400

    Create generators.js

---
+++ generators.js
@@ -0,0 +1,14 @@
+document.title = 'Generators';
+
+var a = [];
+
+for (i = 0; i < 1; i++) {
+    a[i] = document.createElement('a');
+}
+
+a[0].innerHTML = 'Random Number Generator';
+a[0].href = 'generators/random-number-generator.html';
+
+for (i = 0; i < 1; i++) {
+    document.body.appendChild(a[i]);
+}
