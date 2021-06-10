c46239102b5329b6ef39c727bae4b27b7916fa2c
ArianKG <72304422+ArianKG@users.noreply.github.com>
2021-06-09 21:26:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ document.title = 'KGIO';
 var a = [];
 var br = [];
 
-for (i = 0; i < 2; i++) {
+for (i = 0; i < 1; i++) {
     a[i] = document.createElement('a');
     br[i] = document.createElement('br');
 }
 
-a[0].innerHTML = 'Generators';
-a[0].href = 'generators.html';
-a[1].innerHTML = 'Kernel Programming';
-a[1].href = 'kernel-programming.html';
+a[0].innerHTML = 'Kernel Programming';
+a[0].href = 'kernel-programming.html';
 
-for (i = 0; i < 2; i++) {
+for (i = 0; i < 1; i++) {
     document.body.appendChild(a[i]);
     document.body.appendChild(br[i]);
 }
